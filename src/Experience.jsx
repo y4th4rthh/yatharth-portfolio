c6f24624e67761163b86f453b4b45437f1a943e0
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-08-03 20:08:07 +0530

    Add files via upload

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,133 +1,61 @@
 import React from 'react';
-import checkmark from './assets/checkmark.png';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { experiences } from '../constants';
+import Footer from './Footer';
+
 
 const Experience = () => {
-    return (
-        <div>
-            {/* Experience Section */}
-            <section id="experience" className="py-20 lg:py-40 px-5 lg:px-40 dark:bg-gray-50 bg-gray-900">
-                <div className="text-center mb-10">
-                    <p className="text-lg font-semibold dark:text-gray-800 text-gray-200">Explore My</p>
-                    <h1 className="text-5xl font-bold my-4 dark:text-gray-900 text-gray-100">Experience</h1>
-                </div>
-                <div className="flex flex-col justify-center gap-8">
-                    <div className="flex flex-col justify-center lg:flex-row gap-8">
-                        <div className="flex-1 p-8 dark:bg-white bg-gray-800 rounded-2xl shadow-lg">
-                            <h2 className="text-2xl font-semibold mb-4 text-center dark:text-gray-900 text-gray-100">Frontend Techstacks</h2>
-                            <div className="flex flex-wrap justify-center gap-4">
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">HTML</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Experienced</p>
-                                    </div>
-                                </div>
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">CSS</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Experienced</p>
-                                    </div>
-                                </div>
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">Reactjs</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Intermediate</p>
-                                    </div>
-                                </div>
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">Tailwind CSS</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Intermediate</p>
-                                    </div>
-                                </div>
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">TypeScript</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Basic</p>
-                                    </div>
-                                </div>
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">Nextjs</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Intermediate</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+  return (
+    <div className='experience bg-black w-screen text-white pt-4 pt-16 overflow-x-hidden' id='experience'>
+      <div className='pt-12 sm:px-16'>
+        <p className='font-light'>MY JOURNEY SO FAR.</p>
+        <h2 className='text-4xl sm:text-5xl font-extrabold mt-2'>Work Experience.</h2>
+      </div>
+      <VerticalTimeline className='mt-9'>
+        {experiences.map((experience) => (
+          <VerticalTimelineElement
+            className="relative vertical-timeline-element--work"
+            contentStyle={{ background: "#1d1836", color: "#fff", }}
+            contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+            date={experience.duration}
+            iconStyle={{ background: '#fff' }}
+            icon={
+              <a className='flex justify-center items-center w-full h-full' href={experience.url} target='_blank'>
+                <img
+                  src={experience.logo}
+                  alt={experience.company}
+                  className='w-[60%] h-[60%] object-contain'
+                />
+              </a>
+            }
+          >
+            <div>
+              <h3 className='text-white text-[24px] font-bold'>{experience.role}</h3>
+              <p
+                className='text-secondary text-[16px] font-semibold'
+                style={{ margin: 0 }}
+              >
+                {experience.company}
+              </p>
+            </div>
 
-                        <div className="flex-1 p-8 dark:bg-white bg-gray-800 rounded-2xl shadow-lg">
-                            <h2 className="text-2xl font-semibold mb-4 text-center dark:text-gray-900 text-gray-100">Backend Techstacks</h2>
-                            <div className="flex flex-wrap justify-center gap-4">
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">Nodejs</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Experienced</p>
-                                    </div>
-                                </div>
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">Python</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Intermediate</p>
-                                    </div>
-                                </div>
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">Java Spring Boot</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Basic</p>
-                                    </div>
-                                </div>
-        
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">Nextjs</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Intermediate</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="flex-1 p-8 dark:bg-white bg-gray-800 rounded-2xl shadow-lg">
-                            <h2 className="text-2xl font-semibold mb-4 text-center dark:text-gray-900 text-gray-100">Known Databases</h2>
-                            <div className="flex flex-wrap justify-center gap-4">
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">MongoDB</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Experienced</p>
-                                    </div>
-                                </div>
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">MySQL</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Experienced</p>
-                                    </div>
-                                </div>
-                                <div className="flex items-center gap-4">
-                                    <img src={checkmark} alt="Experience icon" className="w-6 h-6" />
-                                    <div>
-                                        <h3 className="text-lg font-semibold dark:text-gray-900 text-gray-100">Firebase</h3>
-                                        <p className="dark:text-gray-800 text-gray-200">Intermediate</p>
-                                    </div>
-                                </div>
-                              
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </section>
-        </div>
-    );
+            <ul className='mt-5 list-disc ml-5 space-y-2'>
+              {experience.points.map((point, index) => (
+                <li
+                  key={`experience-point-${index}`}
+                  className='text-white-100 text-[14px] pl-1 tracking-wider'
+                >
+                  {point}
+                </li>
+              ))}
+            </ul>
+          </VerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
+      <Footer/>
+    </div>
+  )
 }
 
 export default Experience;
