224238ed3237e17115fdb5bb504c0c875e6f5bbf
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-08-03 20:08:33 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,20 +1,4 @@
 
-export const services = [
-    {
-        title: "Technology Enthusiast",
-        icon: 'https://csgeeek.github.io/web.png',
-    },
-    {
-        title: "Web Developer",
-        icon: 'https://csgeeek.github.io/web.png',
-    },
-    {
-        title: "Photographer",
-        icon: 'https://csgeeek.github.io/web.png',
-    },
-];
-
-export const name = 'Yatharth';
 
 // export const experiences = [
 //     {
