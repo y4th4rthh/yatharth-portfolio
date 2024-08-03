a73359c9d068684c6f5f83e6cc862cc90b590a99
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-08-03 19:33:22 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -67,8 +67,9 @@ const Footer = () => {
     onClick={toggleDarkMode}
     className={`transition ease-in-out duration-300 rounded-md ${isDarkMode ? 'text-black' : 'text-gray-200'}`}
     aria-label="Toggle dark mode"
+      style={{marginTop: '4px'}}
 >
-    {isDarkMode ? <BsSun className='text-xl' /> : <BsMoon className='text-xl' />}
+    {isDarkMode ? <BsMoon className='text-xl' /> : <BsSun className='text-xl' />}
 </button>
 
 
