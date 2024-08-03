3a6ffbd9abe9afdd0fc850e332893ea061f9c1cb
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-08-03 18:58:18 +0530

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import emaill from './assets/email.png';
 // import location from './assets/location.png';
 
 const Contact = () => {
