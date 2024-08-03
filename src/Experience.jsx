3c7ba262009e258c3a5274571d0f6838440500c9
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-08-03 20:10:29 +0530

    Update Experience.jsx

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import { experiences } from '../constants';
+import { experiences } from './constants';
 import Footer from './Footer';
 
 
