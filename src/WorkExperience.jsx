4eafb4dfd7074035723e226f70bd0a82d9c38c46
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-08-07 20:33:44 +0530

    Update WorkExperience.jsx

--- src/WorkExperience.jsx
+++ src/WorkExperience.jsx
@@ -1,3 +1,5 @@
+//Not used but may add it later
+
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
