710707f943ef30922d675a1e43b9fddde95027b1
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-09-30 02:04:36 +0530

    Update Project.jsx

--- src/Project.jsx
+++ src/Project.jsx
@@ -15,8 +15,8 @@ const Project = () => {
                 </div>
                 <div className="flex flex-col justify-center lg:flex-row mb-4 gap-8">
     <div className="w-full lg:w-1/3  dark:bg-white bg-gray-900 rounded-2xl shadow-lg">
-        <img src={project1} alt="Gujarat Entrance Exam" className="w-full h-64 object-cover rounded-2xl mb-4 border dark:border-gray-300 border-gray-700" />
-        <div className="sm:p-8 p-2">
+        <img src={project1} alt="Gujarat Entrance Exam" className="w-full h-64 object-cover rounded-t-2xl mb-4 border dark:border-gray-300 border-gray-700" />
+        <div className="sm:p-8 p-4">
             <h2 className="text-xl font-semibold mb-4 dark:text-gray-900 text-gray-100">Gujarat Entrance Exam</h2>
         <p className="dark:text-gray-700 text-gray-300 mb-4">
             • Gujarat Entrance Exam is an exam portal designed using Vite.js and Java Sprint Boot along with Tailwind CSS. It employs modern technologies to provide a user-friendly experience, automating the allocation of exam seats and issuance of hall tickets post-registration.
@@ -25,8 +25,8 @@ const Project = () => {
         </div> 
     </div>
     <div className="w-full lg:w-1/3  dark:bg-white bg-gray-900 rounded-2xl shadow-lg">
-        <img src={project2} alt="Quick Resume" className="w-full h-64 object-cover rounded-2xl mb-4 border dark:border-gray-300 border-gray-700" />
-        <div className="sm:p-8 p-2">
+        <img src={project2} alt="Quick Resume" className="w-full h-64 object-cover rounded-t-2xl mb-4 border dark:border-gray-300 border-gray-700" />
+        <div className="sm:p-8 p-4">
             <h2 className="text-xl font-semibold mb-4 dark:text-gray-900 text-gray-100">Quick Resume</h2>
         <p className="dark:text-gray-700 text-gray-300 mb-4">
             • Quick Resume is a web-based application designed to automate the creation of resumes using Vite.js and Node.js technologies. It provides users with a streamlined experience for generating professional resumes tailored to their specific needs.
@@ -38,8 +38,8 @@ const Project = () => {
 
 <div className="flex flex-col justify-center lg:flex-row mt-4 gap-8">
     <div className="w-full lg:w-1/3  dark:bg-white bg-gray-900 rounded-2xl shadow-lg">
-        <img src={project3} alt="Time Guardian" className="w-full h-64 object-cover rounded-2xl mb-4 border dark:border-gray-300 border-gray-700" />
-        <div className="sm:p-8 p-2">
+        <img src={project3} alt="Time Guardian" className="w-full h-64 object-cover rounded-t-2xl mb-4 border dark:border-gray-300 border-gray-700" />
+        <div className="sm:p-8 p-4">
              <h2 className="text-xl font-semibold mb-4 dark:text-gray-900 text-gray-100">Time Guardian</h2>
         <p className="dark:text-gray-700 text-gray-300 mb-4">
             • Time Guardian is a comprehensive employee management system designed to streamline administrative tasks and enhance workplace efficiency using modern technologies. It offers secure work hour monitoring and fosters transparent communication between managers and employees.
@@ -49,8 +49,8 @@ const Project = () => {
        
     </div>
     <div className="w-full lg:w-1/3  dark:bg-white bg-gray-900 rounded-2xl shadow-lg">
-        <img src={project4} alt="I Can Compile ahh Editor" className="w-full h-64 object-cover rounded-2xl mb-4 border dark:border-gray-300 border-gray-700" />
-        <div className="sm:p-8 p-2">
+        <img src={project4} alt="I Can Compile ahh Editor" className="w-full h-64 object-cover rounded-t-2xl mb-4 border dark:border-gray-300 border-gray-700" />
+        <div className="sm:p-8 p-4">
              <h2 className="text-xl font-semibold mb-4 dark:text-gray-900 text-gray-100">I Can Compile ahh Editor</h2>
         <p className="dark:text-gray-700 text-gray-300 mb-4">
             • "I Can Compile ahh Editor" is an online text editor that supports Python, HTML, and CSS code compilation. It offers real-time preview, syntax highlighting, file management, and a responsive design. Users can securely write, compile, and share code, making it perfect for collaborative projects.
