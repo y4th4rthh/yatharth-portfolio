1c11a77ebdf002ff70d7a180fd9e7a41d54bbe2d
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-09-30 01:59:08 +0530

    Update Project.jsx

--- src/Project.jsx
+++ src/Project.jsx
@@ -14,7 +14,7 @@ const Project = () => {
                     <h1 className="text-5xl font-bold my-4 dark:text-gray-900 text-gray-100">Projects</h1>
                 </div>
                 <div className="flex flex-col justify-center lg:flex-row mb-4 gap-8">
-    <div className="w-full lg:w-1/3  dark:bg-white bg-gray-900 rounded-xl shadow-lg">
+    <div className="w-full lg:w-1/3  dark:bg-white bg-gray-900 rounded-2xl shadow-lg">
         <img src={project1} alt="Gujarat Entrance Exam" className="w-full h-64 object-cover rounded-2xl mb-4 border dark:border-gray-300 border-gray-700" />
         <div className="sm:p-8 p-2">
             <h2 className="text-xl font-semibold mb-4 dark:text-gray-900 text-gray-100">Gujarat Entrance Exam</h2>
@@ -37,21 +37,27 @@ const Project = () => {
 </div>
 
 <div className="flex flex-col justify-center lg:flex-row mt-4 gap-8">
-    <div className="w-full lg:w-1/3 sm:p-8 p-2 dark:bg-white bg-gray-900 rounded-2xl shadow-lg">
+    <div className="w-full lg:w-1/3  dark:bg-white bg-gray-900 rounded-2xl shadow-lg">
         <img src={project3} alt="Time Guardian" className="w-full h-64 object-cover rounded-2xl mb-4 border dark:border-gray-300 border-gray-700" />
-        <h2 className="text-xl font-semibold mb-4 dark:text-gray-900 text-gray-100">Time Guardian</h2>
+        <div className="sm:p-8 p-2">
+             <h2 className="text-xl font-semibold mb-4 dark:text-gray-900 text-gray-100">Time Guardian</h2>
         <p className="dark:text-gray-700 text-gray-300 mb-4">
             • Time Guardian is a comprehensive employee management system designed to streamline administrative tasks and enhance workplace efficiency using modern technologies. It offers secure work hour monitoring and fosters transparent communication between managers and employees.
         </p>
         <a href="https://github.com/y4th4rthh/time-guardian" className="dark:text-blue-500 text-blue-300 dark:hover:text-blue-700 hover:text-blue-400">View Project</a>
+        </div>
+       
     </div>
-    <div className="w-full lg:w-1/3 sm:p-8 p-2 dark:bg-white bg-gray-900 rounded-2xl shadow-lg">
+    <div className="w-full lg:w-1/3  dark:bg-white bg-gray-900 rounded-2xl shadow-lg">
         <img src={project4} alt="I Can Compile ahh Editor" className="w-full h-64 object-cover rounded-2xl mb-4 border dark:border-gray-300 border-gray-700" />
-        <h2 className="text-xl font-semibold mb-4 dark:text-gray-900 text-gray-100">I Can Compile ahh Editor</h2>
+        <div className="sm:p-8 p-2">
+             <h2 className="text-xl font-semibold mb-4 dark:text-gray-900 text-gray-100">I Can Compile ahh Editor</h2>
         <p className="dark:text-gray-700 text-gray-300 mb-4">
             • "I Can Compile ahh Editor" is an online text editor that supports Python, HTML, and CSS code compilation. It offers real-time preview, syntax highlighting, file management, and a responsive design. Users can securely write, compile, and share code, making it perfect for collaborative projects.
         </p>
         <a href="https://github.com/y4th4rthh/text-editor-app" className="dark:text-blue-500 text-blue-300 dark:hover:text-blue-700 hover:text-blue-400">View Project</a>
+        </div>
+       
     </div>
 </div>
 
