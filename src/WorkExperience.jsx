073f13b3966e00f02f623cabcb82c0c253ec3c59
PATEL YATHARTH <114477915+y4th4rthh@users.noreply.github.com>
2025-02-16 18:01:52 +0530

    Small tweak

--- src/WorkExperience.jsx
+++ src/WorkExperience.jsx
@@ -121,7 +121,6 @@ import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import { FaStar } from 'react-icons/fa';
-import { FaStar } from 'react-icons/fa';
 
 const WorkExperience = () => {
   return (
